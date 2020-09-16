fa98ca13c9ce59997d4b3e2f09022fdf0edd11be
adam <adam@harpur.io>
2020-09-16 09:12:30 +0100

    chore: added readme section

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -6,6 +6,7 @@ module.exports = {
     nav: [
       { text: "Home", link: "/" },
       { text: "Blog", link: "/blog/" },
+      { text: "README", link: "/readme/" },
     ],
     directories: [
       {
