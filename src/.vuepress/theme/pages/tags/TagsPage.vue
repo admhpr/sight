746fb9dccfbb8abb1413ad78f39919ee3a99a605
adam <adam@harpur.io>
2020-09-29 08:35:11 +0100

    feat: setup tag iteration

--- src/.vuepress/theme/pages/tags/TagsPage.vue
+++ src/.vuepress/theme/pages/tags/TagsPage.vue
@@ -1,6 +1,13 @@
 <template>
   <main class="home" aria-labelledby="main-title">
-    <section>TAGS</section>
+    <section>
+      <span
+        v-for="tag in Object.keys(tags)"
+        :key="tag"
+        class="text-gray-500 bg-gray-200 h-10 rounded-full px-3 py-1 text-xs select-none"
+        >{{ tag }}</span
+      >
+    </section>
   </main>
 </template>
 
@@ -8,12 +15,26 @@
 export default {
   computed: {
     tags() {
-      return [];
+      const allTags = {};
+      this.postsTags.forEach(({ post, tags }) => {
+        for (const tag of tags) {
+          if (!Object.keys(allTags).includes(tag)) {
+            allTags[tag] = [];
+          }
+          allTags[tag].push(post);
+        }
+      });
+      return allTags;
     },
     postsTags() {
       return this.$site.pages
         .filter((x) => x.path.startsWith("/blog/") && !x.frontmatter.blog_index)
-        .map((post) => post.frontmatter);
+        .map((post) => ({
+          post,
+          tags: post.frontmatter.tags
+            ? post.frontmatter.tags.map((tag) => `${tag}`.toLowerCase())
+            : [],
+        }));
     },
   },
 };
