6b306b7fd87c4dbce67d3f739ed6fbab6ccbe6e7
adam <adam@harpur.io>
2020-10-24 08:53:10 +0100

    chore: change 404 text

--- src/.vuepress/theme/layouts/404.vue
+++ src/.vuepress/theme/layouts/404.vue
@@ -3,7 +3,7 @@
     <div class="theme-default-content">
       <h1>404</h1>
       <blockquote>{{ getMsg() }}</blockquote>
-      <router-link to="/">Take me home.</router-link>
+      <router-link to="/">Take me home</router-link>
     </div>
   </div>
 </template>
